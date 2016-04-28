e8361f7db6f93992e13a718b75f5e733ed50470a
xiaojieLee <xjie.lee93@gmail.com>
2016-04-28 11:35:33 +0800

    fixed all compile error

--- fastcode.cu
+++ fastcode.cu
@@ -1,12 +1,22 @@
+#include <cuda.h>
+#include <cuda_runtime.h>
+#include <opencv2/core/core.hpp>
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/core/cuda_devptrs.hpp"
+#include "opencv2/core/cuda_types.hpp"
+#include "opencv2/cvconfig.h"
+#include "opencv2/cudaarithm.hpp"
 #include "fastcode.h"
+
+using namespace cv;
+using namespace cv::cuda;
+using namespace std;
 
 // reference: http://stackoverflow.com/questions/24613637/custom-kernel-gpumat-with-float
 
-using namespace cv;
-using namespace std;
-
 namespace fastcode{
-    __global__ void maskShowKernel(const cuda::PtrStepSz<uchar> mask, cuda::PtrStepSz<uchar> mask4show){
+    __global__ void maskShowKernel(const PtrStepSz<uchar> mask, PtrStepSz<uchar> mask4show){
         int x = blockIdx.x * blockDim.x + threadIdx.x;
         int y = blockIdx.y * blockDim.y + threadIdx.y;
         if(x < mask.cols && y < mask.rows){
@@ -27,9 +37,11 @@ namespace fastcode{
     }
 
     void maskShowCaller(const Mat & mask, Mat & mask4show){
-        const cuda::GpuMat gmask = mask.getGpuMat();
+        GpuMat gmask;
+	gmask.upload(mask);
         mask4show.create(mask.size(), CV_8UC1);
-        cuda::GpuMat gmask4show = mask4show.getGpuMat();
+        GpuMat gmask4show;
+	gmask4show.upload(mask4show);
 
         dim3 DimBlock(16,16);
         dim3 DimGrid(static_cast<int>(std::ceil(mask.size().width /
@@ -37,5 +49,6 @@ namespace fastcode{
                         static_cast<int>(std::ceil(mask.size().height / 
                         static_cast<double>(DimBlock.y))));
         maskShowKernel<<<DimGrid, DimBlock>>>(gmask, gmask4show);
+	gmask4show.download(mask4show);
     }
 }
--- fastcode.h
+++ fastcode.h
@@ -1,10 +1,6 @@
-#include <opencv2/core/core.hpp>
-#include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
-#include <opencv2/gpu/gpu.hpp>
 
 using namespace cv;
-
 namespace fastcode{
     void maskShowCaller(const Mat & mask, Mat & mask4show);
 }
