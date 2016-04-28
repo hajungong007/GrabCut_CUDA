fac1e5780c903c66b175c6669238633c58826f69
lidajian <li.dajian@foxmail.com>
2016-04-29 05:16:18 +0800

    threshold uses cuda

--- mainFunc.cpp
+++ mainFunc.cpp
@@ -46,7 +46,7 @@ int main()
 		Mat mask;
 		// begin segmentation
 		// Sample 1:
-		segByimgDiff_color(colorImg, bgColorImg, mask, bgdGMM_C, fgdGMM_C, bgdGMM_diff, fgdGMM_diff);
+		segByimgDiff_color(colorImg, colorImgs[0], colorImgs[1], colorImgs[2], bgColorImg, mask, bgdGMM_C, fgdGMM_C, bgdGMM_diff, fgdGMM_diff);
 		// show result
 		Mat resultC;
 		segResultShow(colorImgs[0], mask, resultCs[0]);
