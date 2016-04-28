1d5f4e85eb0cbbed277d8607cdd20ba4ee160fad
lidajian <li.dajian@foxmail.com>
2016-04-28 17:15:38 +0800

    add timer for cpu

--- mainFunc.cpp
+++ mainFunc.cpp
@@ -10,28 +10,29 @@ using namespace std;
 using namespace cv;
 
 int main()
-{		
-	// sample of  color img and imgDiff	
+{
+    double starttime = wtime();
+	// sample of  color img and imgDiff
 	string colorImgSample_filename = "samples/color.png";
 	Mat colorImgSample = imread(colorImgSample_filename, CV_LOAD_IMAGE_COLOR);
 	string imgDiffSample_filename = "samples/diff.png";
-	Mat imgDiffSample = imread(imgDiffSample_filename, CV_LOAD_IMAGE_COLOR);	
+	Mat imgDiffSample = imread(imgDiffSample_filename, CV_LOAD_IMAGE_COLOR);
 	string maskImgSample_filename =  "samples/mask.png";
-	Mat maskSample = imread(maskImgSample_filename, CV_LOAD_IMAGE_GRAYSCALE );	
+	Mat maskSample = imread(maskImgSample_filename, CV_LOAD_IMAGE_GRAYSCALE );
 
 	// learn bgdGMM & fgdGMM
-	Mat bgdModelC, fgdModelC;	
+	Mat bgdModelC, fgdModelC;
 	Mat bgdModelDiff, fgdModelDiff;
 	learnGMMsFromSample( colorImgSample, maskSample, bgdModelC, fgdModelC );
 	learnGMMsFromSample( imgDiffSample, maskSample, bgdModelDiff, fgdModelDiff );
 
-	string bgColorImg_filename = "data/" + convertInt(101) + ".png"; // NOTICE: This is background image!	
+	string bgColorImg_filename = "data/" + convertInt(101) + ".png"; // NOTICE: This is background image!
 	Mat bgColorImg = imread(bgColorImg_filename, CV_LOAD_IMAGE_COLOR);
-	for (int i = 201; i < 824; i = i+1 ) // NOTICE: These are the frames which need to be segmented
+	for (int i = 201; i < 300; i = i+1 ) // NOTICE: These are the frames which need to be segmented
 	{
-		string colorImg_filename = "data/" + convertInt(i) + ".png";			
+		string colorImg_filename = "data/" + convertInt(i) + ".png";
 		Mat colorImg = imread(colorImg_filename, CV_LOAD_IMAGE_COLOR);
-		
+
 		Mat mask;
 		// begin segmentation
 		// Sample 1:
@@ -45,22 +46,25 @@ int main()
 		imwrite("results/"+convertInt(i)+" 0.5_0.5_0.5_0.5_mask"+".png", mask4show);
 
 
-		// Sample 2:
-		segByimgDiff_color(colorImg, bgColorImg, mask, bgdModelC, fgdModelC, bgdModelDiff, fgdModelDiff,1,0,1,0);
-		// show result
-		segResultShow(colorImg, mask, resultC);
-		imwrite("results/"+convertInt(i)+" 1_0_1_0"+".png", resultC);
-		maskBinary(mask, mask4show);
-		imwrite("results/"+convertInt(i)+" 1_0_1_0_mask"+".png", mask4show);
+//		// Sample 2:
+//		segByimgDiff_color(colorImg, bgColorImg, mask, bgdModelC, fgdModelC, bgdModelDiff, fgdModelDiff,1,0,1,0);
+//		// show result
+//		segResultShow(colorImg, mask, resultC);
+//		imwrite("results/"+convertInt(i)+" 1_0_1_0"+".png", resultC);
+//		maskBinary(mask, mask4show);
+//		imwrite("results/"+convertInt(i)+" 1_0_1_0_mask"+".png", mask4show);
 
-		// Sample 3:
-		segByimgDiff_color(colorImg, bgColorImg, mask, bgdModelC, fgdModelC, bgdModelDiff, fgdModelDiff,0,1,0,1);
-		// show result
-		segResultShow(colorImg, mask, resultC);
-		imwrite("results/"+convertInt(i)+" 0_1_0_1"+".png", resultC);
-		maskBinary(mask, mask4show);
-		imwrite("results/"+convertInt(i)+" 0_1_0_1_mask"+".png", mask4show);
+//		// Sample 3:
+//		segByimgDiff_color(colorImg, bgColorImg, mask, bgdModelC, fgdModelC, bgdModelDiff, fgdModelDiff,0,1,0,1);
+//		// show result
+//		segResultShow(colorImg, mask, resultC);
+//		imwrite("results/"+convertInt(i)+" 0_1_0_1"+".png", resultC);
+//		maskBinary(mask, mask4show);
+//		imwrite("results/"+convertInt(i)+" 0_1_0_1_mask"+".png", mask4show);
 	}
+    double endtime = wtime();
+
+    cout<< "Time used: "<< (starttime-endtime) << "milliseconds\n";
 
 	waitKey();
 	return 0;
